7b8cc01dbb5656634c70d43f0e2fc5a3fff477b2
Jose Garcia <josegarcia19809@gmail.com>
2023-11-30 08:04:32 -0600

    Modificando un Dataframe

--- inventario_ferreteria.py
+++ inventario_ferreteria.py
@@ -1,3 +1,6 @@
+"""
+Modificar dataframes
+"""
 import pandas as pd
 
 
@@ -9,9 +12,11 @@ df_inventario = pd.read_csv("datax/inventario_ferreteria.csv")
 print(df_inventario)
 
 imprimir_linea()
+# Se añade una columna para indicar la cantidad
 df_inventario["Cantidad"] = [12, 15, 20, 30, 50]
 print(df_inventario)
 
 imprimir_linea()
+# Se añade una columna en el indice 2 para indicar el descuento
 df_inventario.insert(2, "Descuento", [8, 5, 10, 12, 5])
 print(df_inventario)
